4a26ec65af734172929d8e7c3cd9675e3a6ae4c4
Ayush <ayushnautiyal1110@gmail.com>
2023-06-22 11:56:12 +0530

    Adding Files

--- Manual_SnakeGame.py
+++ Manual_SnakeGame.py
@@ -1,4 +1,8 @@
 import pygame, random, sys
+import streamlit as st
+st.title("==== Welcome to Snake Game ====")
+@st.cache
+
 from pygame.locals import *
 def collide(x1, x2, y1, y2, w1, w2, h1, h2):
 	if x1+w1>x2 and x1<x2+w2 and y1+h1>y2 and y1<y2+h2:
